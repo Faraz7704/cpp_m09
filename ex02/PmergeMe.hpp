a1dc5ccdd64b66a122ac588799ba96657d6dc06f
faraz7704 <faraz7704@gmail.com>
2024-05-22 23:14:34 +0400

    done

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -1,20 +1,46 @@
 #pragma once
 
-#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
 
+/* Type definitions */
+typedef std::vector<int> int_vector;
+
+/* Class definition */
 template<typename Container>
-class PmergeMe {
-	private:
-		static const int INSERTION_THRESHOLD = 10;
+class PmergeMe
+{
+    public:
+        typedef typename Container::value_type      value_type;
+        typedef typename Container::size_type       size_type;
+        typedef std::pair <value_type, value_type>  type_pair;
+        typedef std::vector <type_pair>             pair_vector;
 
-		static void merge(Container& container, typename Container::iterator left, typename Container::iterator mid, typename Container::iterator right);
+    private:
+        Container   _data;
+        value_type  _last;
+        double      _time;
 
-		static void insertionSort(Container& container, typename Container::iterator left, typename Container::iterator right);
+        int_vector  generate_indexes(size_t n);
+        pair_vector generate_pairs(Container &data);
+        void        sort_pairs(pair_vector &pairs);
+        int         binary_search(int target);
+        int         ft_stoa(const char *str);
+        bool        is_number(const std::string& s);
 
-		static void mergeInsertionSort(Container& container, typename Container::iterator left, typename Container::iterator right);
+    public:
+        PmergeMe();
+        explicit PmergeMe(char **data);
+        PmergeMe(const PmergeMe &other);
+        PmergeMe &operator=(const PmergeMe &other);
 
-	public:
-		static void sort(Container& container);
+        ~PmergeMe();
+
+        void sort();
+        void print_data() const;
+        void benchmark() const;
 };
-
-#include "PmergeMe.cpp"
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,96 +1,32 @@
-#include <iostream>
-#include <vector>
-#include <deque>
-#include <sstream>
-#include <ctime>
-
 #include "PmergeMe.hpp"
 
-namespace
+int main(int argc, char** argv)
 {
-	template <class T>
-	void print_okey(const T &c)
-	{
-		typedef typename T::const_iterator const_iterator;
 
-		const_iterator prev = c.begin();
-		const_iterator it = c.begin() + 1;
-		for (; it != c.end(); ++it) {
-			if (*prev > *it) {
-				std::cout << "KO :(" << std::endl;
-				return;
-			}
-		}
-		std::cout << "OK :D" << std::endl;
-	}
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " <positive integer sequence>" << std::endl;
+        return EXIT_FAILURE;
+    }
 
-	template <class T>
-	void print_container(const std::string &mesg, const T &c)
-	{
-		typedef typename T::const_iterator const_iterator;
+    try {
+        PmergeMe<std::vector<int> > v(argv + 1);
 
-		std::cout << mesg;
-		for (const_iterator it = c.begin(); it != c.end(); ++it)
-			std::cout << *it << " ";
-		std::cout << std::endl;
-	}
+        std::cout << "Before:\t";
+        v.print_data();
 
-	template <class T>
-	double bench_container(T &c)
-	{
-		std::clock_t start = std::clock();
-		PmergeMe::sort(c);
-		std::clock_t end = std::clock();
-		double elapsed = 1000000.0 * (end - start) / CLOCKS_PER_SEC;
-		return elapsed;
-	}
-}
+        v.sort();
 
-int main(int argc, char *argv[])
-{
-	if (argc < 2) {
-		std::cout << "Error: No arguments provided.\n";
-		return 1;
-	}
+        PmergeMe<std::deque<int> > dq(argv + 1);
+        dq.sort();
 
-	std::vector<int> v;
-	std::deque<int> d;
+        std::cout << "After:\t";
+        dq.print_data();
 
-	for (int i = 1; i < argc; ++i) {
-		std::stringstream ss(argv[i]);
-		int num;
-		if (!(ss >> num) || !ss.eof()) {
-			std::cout << "Error: Invalid input.\n";
-			return 1;
-		}
-		if (num < 0) {
-			std::cout << "Error: Negative numbers are not allowed.\n";
-			return 1;
-		}
-		v.push_back(num);
-		d.push_back(num);
-	}
+        v.benchmark();
+        dq.benchmark();
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
 
-	print_container("Before: ", v);
-
-	double time_vector = bench_container(v);
-	double time_deque = bench_container(d);
-
-	print_container("After: ", v);
-
-	std::cout << "Time to process a range of "
-	          << v.size()
-	          << " elements with std::vector : "
-	          << time_vector
-	          << " us\n";
-
-	std::cout << "Time to process a range of "
-	          << d.size()
-	          << " elements with std::deque : "
-	          << time_deque
-	          << " us\n";
-
-	print_okey(v);
-
-	return 0;
+    return EXIT_SUCCESS;
 }
